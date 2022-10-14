a9c31065011bd03ce1cef704050417911ed74b80
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-10-15 01:37:32 +0300

    feat: add component Registration.tsx

--- src/component/Registration/Registration.tsx
+++ src/component/Registration/Registration.tsx
@@ -1,10 +1,17 @@
+import {Button, TextField} from '@mui/material';
 import React from 'react';
 import s from "./registration.module.scss"
 
 export const Registration = () => {
     return (
         <div className={s.registrationContainer}>
-            Registration
+            <div className={s.label}>
+                Sign Up
+            </div>
+            <TextField id="outlined-basic" label="Email" variant="outlined" sx={{p: 1}}/>
+            <TextField id="outlined-basic" label="Password" variant="outlined" sx={{p: 1}}/>
+            <TextField id="outlined-basic" label="Confirm password" variant="outlined" sx={{p: 1}}/>
+            <Button variant="outlined">Sign Up</Button>
         </div>
     );
 };
