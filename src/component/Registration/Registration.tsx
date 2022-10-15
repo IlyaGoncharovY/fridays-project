617b0ff350178d162c142eff6794ae96874e2c56
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-10-15 23:08:17 +0300

    fix: add validate formik Registration.tsx

--- src/component/Registration/Registration.tsx
+++ src/component/Registration/Registration.tsx
@@ -4,6 +4,12 @@ import React from 'react';
 import {useAppDispatch, useAppSelector } from '../../bll/hook/hook';
 import s from "./registration.module.scss"
 
+type formikErrorType = {
+    email?: string,
+    password?: string,
+    confirmPassword?: string
+}
+
 export const Registration = () => {
 
     const subscribe = useAppSelector(store => store.registration.subscribe)
@@ -14,6 +20,28 @@ export const Registration = () => {
             email: '',
             password: "",
             confirmPassword: ""
+        },
+        validate: (values) => {
+
+            const errors: formikErrorType = {}
+
+            if (!values.email) {
+                errors.email = "Required"
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = "Invalid email address"
+            }
+
+            if (!values.password) {
+                errors.password = "Required"
+            } else if (values.password.length < 5) {
+                errors.password = "Invalid password"
+            }
+
+            if (values.password !== values.confirmPassword) {
+                errors.confirmPassword = "passwords don't match"
+            }
+
+            return errors
         },
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
@@ -37,20 +65,25 @@ export const Registration = () => {
                         sx={{p: 1}}
                         {...formik.getFieldProps("email")}
                     />
+                    {formik.touched.email && <div>{formik.errors.email}</div>}
                     <TextField
                         id={"password"}
+                        type={"password"}
                         label="Password"
                         variant="outlined"
                         sx={{p: 1}}
                         {...formik.getFieldProps("password")}
                     />
+                    {formik.touched.password && <div>{formik.errors.password}</div>}
                     <TextField
                         id={"confirmPassword"}
+                        type={"password"}
                         label="Confirm password"
                         variant="outlined"
                         sx={{p: 1}}
                         {...formik.getFieldProps("confirmPassword")}
                     />
+                    {formik.touched.confirmPassword && <div>{formik.errors.confirmPassword}</div>}
                     <Button type={"submit"} variant="outlined">Sign Up</Button>
                 </div>
             </form>
