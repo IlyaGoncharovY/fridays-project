e0658b775450e5661876db086f0a3fad5b1ed5e0
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-10-19 00:17:17 +0300

    fix: deleting commented code

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,7 +1,7 @@
 import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
 import {authReducer, setAuthACType} from "./reducers/authReducer";
 import thunkMiddleware, {ThunkAction, ThunkDispatch} from "redux-thunk"
-import {cgangeStatusRegistrationACType, registrationReducer} from "./reducers/registration-Reducer";
+import {changeStatusRegistrationACType, registrationReducer} from "./reducers/registration-Reducer";
 import {ActionLoginType, loginReducer} from "./reducers/loginReducer";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {setUserACType} from "./reducers/profileReducer";
@@ -13,13 +13,11 @@ export type StoreType = typeof store
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type allReducersType = setAuthACType | setUserACType | cgangeStatusRegistrationACType | ActionLoginType
+export type allReducersType = setAuthACType | setUserACType | changeStatusRegistrationACType | ActionLoginType
 export type AppDispatch = ThunkDispatch<RootState, unknown, allReducersType>
-// export type AppDispatch = any
-// export const useAppDispatch = () => useDispatch<AppDispatch>()
+
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
     RootState, unknown, allReducersType>
-// export const useAppSelector : TypedUseSelectorHook<RootState> = useSelector
 
 const rootReducer = combineReducers({
     registration: registrationReducer,
