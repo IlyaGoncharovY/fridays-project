3e019cb1c926f1828074dfe775df3ac1809fc0a7
Dima <d2.grinevich@gmail.com>
2022-10-19 05:13:19 +0300

    feat(initializing): created initializingTC

--- src/bll/reducers/authReducer.ts
+++ src/bll/reducers/authReducer.ts
@@ -1,3 +1,9 @@
+import {AppThunk} from "../store";
+import {registartionAPI} from "../../api/regApi";
+import {loginAC} from "./loginReducer";
+import {errorUtil} from "../../utils/error-util";
+import {AxiosError} from "axios";
+
 const initialState = {
     isAuth: false,
 
@@ -7,7 +13,7 @@ type initialStateType = typeof initialState
 type AuthReducerType = setAuthACType
 
 
-export const authReducer = (state:initialStateType = initialState, action:AuthReducerType):initialStateType=> {
+export const authReducer = (state: initialStateType = initialState, action: AuthReducerType): initialStateType => {
     switch (action.type) {
         case "AUTH": {
             return {
@@ -22,9 +28,26 @@ export const authReducer = (state:initialStateType = initialState, action:AuthRe
 
 //AC
 export type setAuthACType = ReturnType<typeof setAuthAC>
-export const setAuthAC = (isAuth:boolean) => {
-  return {
-      type: "AUTH",
-      isAuth
-  } as const
+export const setAuthAC = (isAuth: boolean) => {
+    return {
+        type: "AUTH",
+        isAuth
+    } as const
+}
+
+//THUNK
+
+export const initializingTC = (): AppThunk => async dispatch => {
+    try {
+        await registartionAPI.me()
+        dispatch(loginAC(true))
+
+    } catch (e) {
+        const error = e as Error | AxiosError<{ error: string }>
+        errorUtil(error, dispatch)
+    } finally {
+        dispatch(setAuthAC(true))
+    }
+
+
 }
