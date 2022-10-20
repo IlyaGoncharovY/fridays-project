8df46180eafbffc53b4e966e5ba25474be7a88c5
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-10-20 23:24:30 +0300

    fix(index): change BrowserRouter on HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-import { Provider } from 'react-redux';
-import { store } from './bll/store';
+import {HashRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
+import {store} from './bll/store';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-    </BrowserRouter>
-
+    <Provider store={store}>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
