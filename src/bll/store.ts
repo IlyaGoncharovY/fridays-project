776cbd9f63759e082733b73b3563806a463be841
Dima <d2.grinevich@gmail.com>
2022-10-20 03:42:07 +0300

    fix(store): added new Actions types

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,10 +1,11 @@
 import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
 import {authReducer, setAuthACType} from "./reducers/authReducer";
 import thunkMiddleware, {ThunkAction, ThunkDispatch} from "redux-thunk"
-import {ErrorReducer} from "./reducers/ErrorReducer";
+import {ActionErrorType, ErrorReducer} from "./reducers/ErrorReducer";
 import {ProfileActionType, profileReducer} from "./reducers/profileReducer";
 import {RegistrationActionType, registrationReducer} from "./reducers/registration-Reducer";
 import {ActionLoginType, loginReducer} from "./reducers/loginReducer";
+import {ActionRecoveryPasswordType, recoveryReducer} from "./reducers/recoveryPasswordReducer";
 
 export type AppStateType = ReturnType<typeof rootReducer>
 export type StoreType = typeof store
@@ -12,7 +13,7 @@ export type StoreType = typeof store
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppActionType = setAuthACType | ProfileActionType | RegistrationActionType | ActionLoginType
+export type AppActionType = setAuthACType | ProfileActionType | RegistrationActionType | ActionLoginType | ActionRecoveryPasswordType | ActionErrorType
 export type AppDispatch = ThunkDispatch<RootState, unknown, AppActionType>
 
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AppActionType>
@@ -22,7 +23,8 @@ const rootReducer = combineReducers({
     profile: profileReducer,
     auth : authReducer,
     login : loginReducer,
-    error : ErrorReducer
+    error : ErrorReducer,
+    recovery : recoveryReducer
 
 })
 
