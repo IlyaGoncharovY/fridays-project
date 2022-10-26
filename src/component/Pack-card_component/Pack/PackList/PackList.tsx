c8ae0ea6b17a791c94c8024b69b19375d3049470
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-10-26 22:49:29 +0300

    feat(PacksModal): add modal window for PackList.tsx

--- src/component/Pack-card_component/Pack/PackList/PackList.tsx
+++ src/component/Pack-card_component/Pack/PackList/PackList.tsx
@@ -1,8 +1,12 @@
-import { Button } from "@mui/material"
-import { PackFilter } from "../PackFilter/PackFilter"
+import {Button} from "@mui/material"
+import {useState} from "react"
+import {PacksModal} from "../../../common/modalWindow/PackModal"
+import {PackFilter} from "../PackFilter/PackFilter"
 import s from "./pack-list.module.scss"
 
+
 export const PackList = () => {
+
     return (
         <div className={s.PackListContainer}>
             <div className={s.PackListHeader}>
@@ -10,7 +14,7 @@ export const PackList = () => {
                     Pack list
                 </div>
                 <div className={s.PackListHeaderButton}>
-                    <Button variant="contained">Add new pack</Button>
+                    <PacksModal title={"Add new pack"}/>
                 </div>
             </div>
             <PackFilter/>
---
+++ src/component/common/modalWindow/PackModal.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {Checkbox, FormControlLabel, TextField } from '@mui/material';
+import s from "./packModal.module.scss"
+import { useState } from 'react';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+type ModalType = {
+    title: string
+}
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } }
+
+export const PacksModal = (props: ModalType) => {
+
+    const [open, setOpen] = useState(false)
+
+    const openHandler = () => {
+        setOpen(true)
+    }
+
+    const closeHandler = () => {
+        setOpen(false)
+    }
+
+    return (
+        <div>
+            <Button onClick={openHandler}>{props.title}</Button>
+            <Modal
+                open={open}
+                onClose={closeHandler}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Add new pack
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        <div className={s.modalBody}>
+                            <TextField id="standard-basic" label="name pack" variant="standard" />
+                            <FormControlLabel control={<Checkbox defaultChecked />} label="Private pack" />
+                        </div>
+                    </Typography>
+                </Box>
+            </Modal>
+        </div>
+    );
+}
