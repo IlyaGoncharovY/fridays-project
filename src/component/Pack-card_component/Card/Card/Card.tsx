09448d0e8de08a5f794ccdae82460f1d2c631de1
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-10-26 23:29:26 +0300

    feat(): add new component Card.tsx

---
+++ src/component/Pack-card_component/Card/Card/Card.tsx
@@ -0,0 +1,46 @@
+import {Icon, IconButton} from "@mui/material"
+import s from "./card.module.scss"
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+type PacksType = {
+    question: string,
+    answer: string,
+    lastUpdated: string,
+    grade: number,
+}
+
+export const Card = (props: PacksType) => {
+
+    const addHandler = () => {
+
+    }
+
+    const editHandler = () => {
+
+    }
+
+    const deleteHandler = () => {
+
+    }
+
+    return (
+        <div className={s.CardContainer}>
+
+            <div className={s.CardBody}>
+                <div>{props.question}</div>
+                <div>{props.answer}</div>
+                <div>{props.lastUpdated}</div>
+                <div>{props.grade}</div>
+                <div>
+                    <AddCircleOutlineIcon onClick={addHandler}/>
+                    <EditIcon onClick={editHandler}/>
+                    <DeleteIcon onClick={deleteHandler}/>
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
