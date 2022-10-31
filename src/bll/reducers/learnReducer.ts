b5e7f8a5a3b029eab24bd42a80c992c4b989e171
Dima <d2.grinevich@gmail.com>
2022-11-01 02:54:03 +0300

    feat(reducer):  learn reducer

---
+++ src/bll/reducers/learnReducer.ts
@@ -0,0 +1,48 @@
+import {cardsAPI} from "../../api/cardsAPI";
+import {AppThunk} from "../store";
+import {editCardGrade} from "./cardsReducer";
+
+const initialState = {
+    packID: "",
+    packName: "",
+
+
+}
+
+export const learnReducer = (state: InitialStateType = initialState, action: LearnActionsType): InitialStateType => {
+    switch (action.type) {
+        case "learn/SET-PACK":
+            return {
+                ...state,
+                packID: action.payload.packID,
+                packName: action.payload.packName
+            }
+        default :
+            return state
+    }
+}
+
+//Actions
+export const setUserPack = (payload: { packID: string, packName: string }) => ({
+    type: "learn/SET-PACK",
+    payload
+} as const)
+
+
+//Thunks
+
+export const setCardsGrade = (id: string, rate: number): AppThunk => async dispatch => {
+    const result = await cardsAPI.updateCardGrate({card_id: id, grade: rate})
+    const {grade, card_id, shots} = result.data.updatedGrade
+
+    dispatch(editCardGrade({grade, card_id, shots}))
+
+}
+
+
+//Type
+
+type InitialStateType = typeof initialState
+export type LearnActionsType =
+    | ReturnType<typeof setUserPack>
+
