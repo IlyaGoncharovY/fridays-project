651832fb4048cdd03bdf8fa4f585871fd495a186
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-11-01 22:35:27 +0300

    feat(DeleteModal): added new component DeleteModal.tsx

--- src/component/common/modalWindow/deleteModal/DeleteModal.tsx
+++ src/component/common/modalWindow/deleteModal/DeleteModal.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import * as React from 'react';
+import {BasicModal} from "../BasicModal";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-const DeleteModal = () => {
+type ModalType = {
+    nameModal: string
+    open: boolean
+    closeHandler: ()=> void
+    thunkCallBack: ()=>void
+    packID?: string
+    name?: string
+}
+
+export const DeleteModal = (props: ModalType) => {
+
     return (
         <div>
-            
+            <BasicModal open={props.open} closeHandler={props.closeHandler}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    {props.nameModal}
+                </Typography>
+                <Typography id="modal-modal-description" sx={{mt: 2}}>
+                    Are you sure you want to delete the deck: {props.name}?
+                    <div>
+                        <Button variant="outlined"
+                                onClick={props.closeHandler}>cancel</Button>
+                        <Button variant="outlined"
+                                onClick={props.thunkCallBack}>delete</Button>
+                    </div>
+                </Typography>
+            </BasicModal>
         </div>
     );
-};
-
-export default DeleteModal;
+}
