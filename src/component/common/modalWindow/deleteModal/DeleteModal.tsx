aa7725d1c2496886aafe7b802451524b7f74e7f7
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-11-01 22:50:19 +0300

    fix(DeleteModal): refactor style button delete

--- src/component/common/modalWindow/deleteModal/DeleteModal.tsx
+++ src/component/common/modalWindow/deleteModal/DeleteModal.tsx
@@ -26,7 +26,7 @@ export const DeleteModal = (props: ModalType) => {
                         <Button variant="outlined"
                                 onClick={props.closeHandler}>cancel</Button>
                         <Button variant="outlined"
-                                onClick={props.thunkCallBack}>delete</Button>
+                                onClick={props.thunkCallBack} style={{backgroundColor:"coral"}}>delete</Button>
                     </div>
                 </Typography>
             </BasicModal>
