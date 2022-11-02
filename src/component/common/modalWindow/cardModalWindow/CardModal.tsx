f8b900b4ee56644d5cf92b11660f2c6d6c5220af
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-11-03 00:16:08 +0300

    fix(CardModal): refactor code CardModal.tsx. added new input

--- src/component/common/modalWindow/cardModalWindow/CardModal.tsx
+++ src/component/common/modalWindow/cardModalWindow/CardModal.tsx
@@ -1,63 +1,48 @@
 import * as React from 'react';
-import {ChangeEvent, useState} from 'react';
+import {ChangeEvent} from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {Checkbox, FormControlLabel, TextField} from '@mui/material';
 import s from "./CardModal.module.scss"
-import {useAppDispatch} from "../../../../bll/hook/hook";
-import {addCardTC} from "../../../../bll/reducers/cardsReducer";
 import {BasicModal} from "../BasicModal";
 
 type ModalType = {
-    title: string
-    nameModal: string
+    open: boolean
+    closeHandler: () => void
+    thunkCallBack: () => void
+    onChangeQuestion: (event: ChangeEvent<HTMLInputElement>) => void
+    onChangeAnswer: (event: ChangeEvent<HTMLInputElement>) => void
+    nameModal?: string
+    nameInput?: string
+    question?: string
+    answer?: string
 }
 
 export const CardModal = (props: ModalType) => {
 
-    const dispatch = useAppDispatch()
-
-    const [open, setOpen] = useState(false)
-    const [question, setQuestion] = useState("")
-
-    const openHandler = () => {
-        setOpen(true)
-    }
-
-    const closeHandler = () => {
-        setOpen(false)
-    }
-
-    const addCardHandler = () => {
-        if (question.trim()) {
-            dispatch(addCardTC(question.trim()))
-        }
-        setQuestion("")
-    }
-
-    const onChangeTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setQuestion(event.currentTarget.value)
-    }
-
     return (
         <div>
-            <Button onClick={openHandler}>{props.title}</Button>
-            <BasicModal open={open} closeHandler={closeHandler}>
+            <BasicModal open={props.open} closeHandler={props.closeHandler}>
                 <Typography id="modal-modal-title" variant="h6" component="h2">
-                    {props.nameModal}
+                    {props.nameModal || props.nameInput}
                 </Typography>
                 <Typography id="modal-modal-description" sx={{mt: 2}}>
                     <div className={s.modalBody}>
                         <TextField id="standard-basic"
-                                   value={question}
-                                   label="enter question"
+                                   value={props.question}
+                                   label="Question"
                                    variant="standard"
-                                   onChange={onChangeTitleHandler}/>
+                                   onChange={props.onChangeQuestion}/>
+                        <TextField id="standard-basic"
+                                   value={props.answer}
+                                   label="Answer"
+                                   variant="standard"
+                                   onChange={props.onChangeAnswer}/>
                         <FormControlLabel control={<Checkbox defaultChecked/>} label="Private pack"/>
                         <Button variant="outlined"
-                                onClick={closeHandler}>cansel</Button>
+                                onClick={props.closeHandler}>exit</Button>
                         <Button variant="outlined"
-                                onClick={addCardHandler}>add</Button>
+                                onClick={props.thunkCallBack}>add</Button>
                     </div>
                 </Typography>
             </BasicModal>
