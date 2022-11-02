84acef5e97828d8d392b5265293fa6c7ac49cc6e
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-11-03 00:14:22 +0300

    fix(CardList): refactor code CardList.tsx. modal window

--- src/component/Pack-card_component/Card/CardList/CardList.tsx
+++ src/component/Pack-card_component/Card/CardList/CardList.tsx
@@ -5,20 +5,56 @@ import s from "./Card-list.module.scss"
 import {PaginationButtons} from "../../../common/Pagination/Pagination";
 import {useAppDispatch, useAppSelector} from "../../../../bll/hook/hook";
 import {changeCardsPages} from "../../../../bll/reducers/pageCardsReducer";
-import {useEffect} from "react";
-import {fetchCardsTC} from "../../../../bll/reducers/cardsReducer";
+import {ChangeEvent, useEffect, useState} from "react";
+import {addCardTC, fetchCardsTC} from "../../../../bll/reducers/cardsReducer";
+import Button from "@mui/material/Button/Button";
 
 
 export const CardList = () => {
     const params = useParams()
+
     const id = params.cardID
     const page = useAppSelector(state => state.cardsPages.countPerPage)
     const totalCount = useAppSelector(state => state.cardsPages.cardsTotalCount)
+
     const dispatch = useAppDispatch()
-    useEffect(() => {if(id){dispatch(fetchCardsTC(id))}}, [])
+
+    const [open, setOpen] = useState(false)
+    const [question, setQuestion] = useState("")
+    const [answer, setAnswer] = useState("")
+
+    useEffect(() => {
+        if (id) {
+            dispatch(fetchCardsTC(id))
+        }
+    }, [])
+
     const setPages = (value: number) => {
         dispatch(changeCardsPages(value))
     }
+
+    const openHandler = () => {
+        setOpen(true)
+    }
+
+    const closeHandler = () => {
+        setOpen(false)
+    }
+
+    const addCardHandler = () => {
+        if (question.trim()) {
+            dispatch(addCardTC(question.trim()))
+        }
+        setQuestion("")
+    }
+
+    const onChangeQuestionHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setQuestion(event.currentTarget.value)
+    }
+    const onChangeAnswerHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setAnswer(event.currentTarget.value)
+    }
+
     return (
         <div className={s.CardListContainer}>
             <NavLink to={"/pack-list"}>return to PackList</NavLink>
@@ -27,7 +63,15 @@ export const CardList = () => {
                     Card list
                 </div>
                 <div className={s.CardListHeaderButton}>
-                    <CardModal title={"Add new card"} nameModal={"Add new card"}/>
+                    <Button onClick={openHandler} variant="contained">{"Add new card"}</Button>
+                    <CardModal
+                        open={open}
+                        closeHandler={closeHandler}
+                        thunkCallBack={addCardHandler}
+                        onChangeQuestion={onChangeQuestionHandler}
+                        onChangeAnswer={onChangeAnswerHandler}
+                        nameModal={"Add new card"}
+                    />
                 </div>
             </div>
             <CardFilter/>
