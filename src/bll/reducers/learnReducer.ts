4f328b03f874493ccceb48b8908a3f3b138bb6b2
Maxim Remez <remezmaxim1992@gmail.com>
2022-11-04 12:14:52 +0300

    fix: add status for learnReducer

--- src/bll/reducers/learnReducer.ts
+++ src/bll/reducers/learnReducer.ts
@@ -1,6 +1,9 @@
 import {cardsAPI} from "../../api/cardsAPI";
 import {AppThunk} from "../store";
 import {editCardGrade} from "./cardsReducer";
+import {setStatusAC} from "./authReducer";
+import {AxiosError} from "axios";
+import {errorUtil} from "../../utils/error-util";
 
 const initialState = {
     packID: "",
@@ -28,9 +31,17 @@ export const setUserPack = (payload: { packID: string, packName: string }) => ({
 
 //TC
 export const setCardsGrade = (id: string, rate: number): AppThunk => async dispatch => {
-    const result = await cardsAPI.updateCardGrade({card_id: id, grade: rate})
-    const {grade, card_id, shots} = result.data.updatedGrade
-    dispatch(editCardGrade({grade, card_id, shots}))
+    try {
+        dispatch(setStatusAC("loading"))
+        const result = await cardsAPI.updateCardGrade({card_id: id, grade: rate})
+        const {grade, card_id, shots} = result.data.updatedGrade
+        dispatch(editCardGrade({grade, card_id, shots}))
+        dispatch(setStatusAC("succeeded"))
+    }
+    catch(e){
+        const error = e as Error | AxiosError<{ error: string }>
+        errorUtil(error, dispatch)
+    }
 
 }
 
